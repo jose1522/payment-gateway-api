6ee0a5cc572392c3a7bed18966fadec1085e5ca3
Jose Andres Moya Porras <jose.91cr@gmail.com>
2020-08-14 18:02:26 -0600

    Adding CORS

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, Depends
 from api.routes import apiRouter
 from .routerDependencies import *
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def create_app():
+    
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(apiRouter, dependencies=[Depends(get_token_header)])
     return app
